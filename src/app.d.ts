f6f4304d99f1a7c7fef43fe17389e6ad4ebf2449
nilsreich <nilsreich@gmail.com>
2023-01-24 22:36:52 +0000

    daisyasd

--- src/app.d.ts
+++ src/app.d.ts
@@ -4,11 +4,10 @@
 // for information about these interfaces
 // and what to do when importing types
 declare namespace App {
-
 	// interface Error {}
 	// interface Platform {}
-  }
-  // See https://kit.svelte.dev/docs/types#app
+}
+// See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
 declare global {
