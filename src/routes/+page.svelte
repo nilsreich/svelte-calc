a6e48ad4d4c9f63cc825e84e22b88f9298c23dac
nilsreich <nilsreich@gmail.com>
2023-01-25 07:30:09 +0000

    Dynamic import mathjs Fixes #2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,9 +12,15 @@
 	};
 
 	const solve = async() => {
-		const { evaluate, format } = await import('mathjs');
-		let temp = await evaluate(input);
-		result = await format(temp, {precision: 14}).toString();
+		var { evaluate, format } = await import('mathjs');
+		
+		// replace invalid characters
+		let temp = input.replaceAll('−', '-');
+		temp = temp.replaceAll(',', '.');
+		temp = temp.replaceAll('×','*')
+
+		let tempmath = await evaluate(temp);
+		result = await format(tempmath, {precision: 14}).toString();
 	};
 </script>
 
@@ -28,9 +34,9 @@
 		</div>
 	</div>
 	<div class="grid grid-cols-4 grow gap-2 py-4">
-		<button on:click={update} data-symbol="(" class="btn btn-ghost text-2xl">(</button>
-		<button on:click={update} data-symbol=")" class="btn btn-ghost text-2xl">)</button>
-		<button on:click={update} data-symbol="^" class="btn btn-ghost text-2xl">^</button>
+		<button on:click={update} class="btn btn-ghost text-2xl">(</button>
+		<button on:click={update} class="btn btn-ghost text-2xl">)</button>
+		<button on:click={update} class="btn btn-ghost text-2xl">^</button>
 		<button on:click={rem} class="btn btn-ghost ">rem</button>
 		<button on:click={update} data-symbol="7" class="btn btn-ghost text-2xl">7</button>
 		<button on:click={update} data-symbol="8" class="btn btn-ghost text-2xl">8</button>
@@ -39,12 +45,12 @@
 		<button on:click={update} data-symbol="4" class="btn btn-ghost text-2xl">4</button>
 		<button on:click={update} data-symbol="5" class="btn btn-ghost text-2xl">5</button>
 		<button on:click={update} data-symbol="6" class="btn btn-ghost text-2xl">6</button>
-		<button on:click={update} data-symbol="-" class="btn btn-ghost text-2xl">-</button>
+		<button on:click={update} data-symbol="−" class="btn btn-ghost text-2xl">−</button>
 		<button on:click={update} data-symbol="1" class="btn btn-ghost text-2xl">1</button>
 		<button on:click={update} data-symbol="2" class="btn btn-ghost text-2xl">2</button>
 		<button on:click={update} data-symbol="3" class="btn btn-ghost text-2xl">3</button>
-		<button on:click={update} data-symbol="*" class="btn btn-ghost text-2xl">*</button>
-		<button on:click={update} data-symbol="." class="btn btn-ghost text-2xl">,</button>
+		<button on:click={update} data-symbol="×" class="btn btn-ghost text-2xl">×</button>
+		<button on:click={update} data-symbol="," class="btn btn-ghost text-2xl">,</button>
 		<button on:click={update} data-symbol="0" class="btn btn-ghost text-2xl">0</button>
 		<button on:click={solve} class="btn btn-ghost text-2xl">=</button>
 		<button on:click={update} data-symbol="/" class="btn btn-ghost text-2xl">/</button>
